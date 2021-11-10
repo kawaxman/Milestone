723070d1b1c84bee0cdb3b0edf5fc4ab104f039f
kawaxman <kawaxman@icloud.com>
2021-11-09 19:13:50 -0700

    Revert "Adding pods + xcworkspace & signup and login functionality implemented and finished"
    This reverts commit a9126d5f468d02b5161a10174632ee8625f22730.

--- Pods/Target Support Files/Pods-milestone-milestoneUITests/Pods-milestone-milestoneUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_milestone_milestoneUITests : NSObject
-@end
-@implementation PodsDummy_Pods_milestone_milestoneUITests
-@end
--- Pods/Target Support Files/Pods-milestone/Pods-milestone-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_milestone : NSObject
-@end
-@implementation PodsDummy_Pods_milestone
-@end
