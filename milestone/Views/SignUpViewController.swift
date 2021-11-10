7f087a1fb8b5d2ff96ead46e7342af90ca03e832
kawaxman <kawaxman@icloud.com>
2021-11-09 19:10:56 -0700

    Adding pods + xcworkspace & signup and login functionality implemented and finished

---
+++ milestone/Views/SignUpViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SignUpViewController.swift
+//  milestone
+//
+//  Created by Kent Waxman on 11/9/21.
+//
+
+import UIKit
+import FirebaseAuth
+import SwiftUI
+
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var reenterPasswordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBAction func signUpAction(_ sender: Any) {
+        if (passwordTextField.text != reenterPasswordTextField.text) {
+            let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        } else {
+            Auth.auth().createUser(withEmail: usernameTextField.text!, password: passwordTextField.text!){ (user, error) in
+                if error == nil {
+                    self.performSegue(withIdentifier: "signUpToTimeline", sender: self)
+                } else{
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                            
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+}
---
+++ milestone/Views/TimelineViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TimelineViewController.swift
+//  milestone
+//
+//  Created by Kent Waxman on 11/9/21.
+//
+
+import UIKit
+import SwiftUI
+
+
+class TimelineViewController: UIViewController {
+    
+}
